02e4b51cf9e6af73a97e4c5d174408edbe402087
Robert Koch <robert@kochie.io>
2023-03-15 02:22:02 +1100

    moved amplify higher

--- src/components/amplify/index.tsx
+++ src/components/amplify/index.tsx
@@ -7,8 +7,6 @@ export const AmplifyClient = () => {
     userPoolId: process.env.NEXT_PUBLIC_USERPOOL_ID,
     userPoolWebClientId: process.env.NEXT_PUBLIC_USERPOOL_CLIENT_ID,
     region: process.env.NEXT_PUBLIC_REGION,
-
-    ssr: true,
   };
 
   Auth.configure(config);
